635d8e46803b2c89af36ccf95710c53a35c8ae78
navs23 <navs@hotmail.co.uk>
2016-05-03 13:48:12 +0000

    www redirect url added

--- server.js
+++ server.js
@@ -70,7 +70,19 @@ process.on('uncaughtException', function (err) {
   console.log(err);
   
   
-})
+});
+
+
+function wwwRedirect(req, res, next) {
+    if (req.headers.host.slice(0, 4) === 'www.') {
+        var newHost = req.headers.host.slice(4);
+        return res.redirect(301, req.protocol + '://' + newHost + req.originalUrl);
+    }
+    next();
+};
+
+
+app.use(wwwRedirect);
 
 //process.stdout.write('\033c');
 
