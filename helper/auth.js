6fd019d7ae9910a79dfda85d495f7640a2982ad1
navs23 <navs@hotmail.co.uk>
2016-08-12 11:06:18 +0000

    auth.js added

---
+++ helper/auth.js
@@ -0,0 +1,21 @@
+
+
+
+(function(auth)
+{
+   function isLoggedIn (req, res, next) {
+    // console.log('isloggedIn');
+    //console.log(isAuthenticated());
+    // if user is authenticated in the session, carry on
+    if (req.isAuthenticated())
+        return next();
+
+    // if they aren't redirect them to the home page
+    res.redirect('/login/');
+}
+ 
+ auth.isLoggedIn= isLoggedIn;
+   
+}
+
+)(module.exports);
