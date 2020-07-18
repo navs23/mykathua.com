d32d663afc001607e788ae0d730dea78c79e0279
naveen sharma <navs@hotmail.co.uk>
2020-07-18 21:35:23 +0100

    Update homeController.js

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,246 +1,185 @@
-/*
-node-cache module added
-*/
-"use strict";
-(function(mediaController) {
-  const data = require("../sql");
-  const twt = require("../helper/tweet.js");
-  //const news = require("../news");
-  const videos = require("../videos");
-  const cacheMiddleware = require("../middleware/cache");
-  const newsMiddleware = require("../middleware/news");
-  //var newsItemCache;
-  let cacheExpiryTime = process.env.cache_expiry_time || 300;
+(function(homeController){
 
-  // landing page;
-  mediaController.init = function(app) {
-    //console.log('db object 1 %s',app.dbNews);
-    routes(app);
-    //news.setNewsdb(app.dbNews);
+  var cheerio = require('cheerio');
+  var dex =require("../helper");
+  var twt =require("../helper/tweet.js");
+  var dal = require("../sql");
+  var cms =require("../sql/cms.js");
+  var emailHelper = require('../helper/mail.js');
+  var Datastore = require('nedb');
+  var geoip = require('geoip-lite');
+
+  
+  var dbStats = new Datastore({ filename:  'stats.db',autoload:true});
+  var url='http://api.openweathermap.org/data/2.5/weather?q=Kathua,in&mode=html&appid=816adfb03ad4efdaee6a6105152c3916';
+ 
+ 
+  homeController.init= function(app){
+      
+       setupRoutes(app);    
+     
+      
   };
+  
+var savewebhits=function(data,cb){
+   dbStats.insert(data, function (err, doc) {
+                  if (err)
+                 cb(err, null);
+                 else
+                 cb(null, doc);
+                  
+              });
+ 
+}
 
-  var routes = function(app) {
-    app.get(
-      "/news/",
-    //  cacheMiddleware(cacheExpiryTime),
-    //  newsMiddleware(),
-      function(req, res, next) {
-        console.log("user agent %s", /mobile/i.test(req.header("user-agent")));
-
-        let db = require("../database");
-        let NewsModel = require("../database/models/newsModel");
-
-        //news.GetNewItemsFromSql(function(err,temp){
-        //   if (err==null)
-        db.get(NewsModel, {})
-          .then(result => {
-           let model= {
-              user: req.user,
-              news: result,
-              title:"Kathua in news, get latest national, state and kathua news on mykathua.com",
-              isMobile: /mobile/i.test(req.header("user-agent"))
-            };
-          console.log(model);
-            // videos.search({searchStr:'kathua'},function(data){
-            res.render("media/news", model
-             );
-          })
-          .catch(err => res.render(err));
-      }
-    );
-    app.get("/videos/:name?", function(req, res, next) {
-      videos.search({ searchStr: req.params.name || "kathua" }, function(data) {
-        res.render("media/videos2", {
-          user: req.user,
-          videos: data,
-          title: "Latest videos trending videos from Kathua @@mykathua.com"
-        });
+var setupRoutes=function(app){
+     app.get("/",function(req,res,next){
+         
+         
+          var ip = req.headers['x-forwarded-for'] ||  req.connection.remoteAddress ||req.socket.remoteAddress || req.connection.socket.remoteAddress;
+          
+          var geo = geoip.lookup(ip);
+          
+          var data={url:req.url,ipaddress:ip,dated:new Date().toGMTString(),geo:geo};
+          savewebhits(data,function(err,res){
+              //console.log(res);
+              
+          });
+          
+           var cmsContent={};
+     
+          cms.getWebsiteContentByPageAndSection({page:'home',section:'top-header'},function(err,value){
+              
+              
+          if (err==null){cmsContent.cmsTopHeader=value[0].content;
+          }
+              cms.getWebsiteContentByPageAndSection({page:'home',section:'flash'},function(err,value){
+                  
+              
+              if (err==null){
+              cmsContent.flashMessage=value[0].content;
+              
+              }
+            
+          });    
+          
       });
-    });
-    app.get("/api/news/", 
-      cacheMiddleware(cacheExpiryTime),
-      newsMiddleware(),
-      function(req, res, next) {
-        console.log(req.url);
-        let db = require("../database");
-        let NewsModel = require("../database/models/newsModel");
-        db.get(NewsModel, {})
-          .then(result => {
-            // videos.search({searchStr:'kathua'},function(data){
-              res.json(result)
-          })
-          .catch(err => res.render(err));
-      }
-      //next();
-    );
+          
+          
+           try {
+                  dex.scrape(url,function(html){
+                  
+                       twt.getTweets(function(err,data){
 
-    app.get("/api/newsfromSQL/", function(req, res, next) {
-      news.GetNewItemsFromSql(function(err, result) {
-        res.send(result);
+                          if (err==null)
+                          {
+                              res.render("home",{user:req.user,weather:html,tweets:data,messages:{},title:"Welcome to mykathua.com",cmsContent:cmsContent,mode:process.env.mode});
+                              //console.log(data);
+                          }
+                          else 
+                          return next();
+                      
+                   });
+              },function(error){return next(error);});
+           }
+           catch(err){
+              
+               console.log(err);
+
+               return next(err);
+           }
+          
+      });   
+         
+      app.get("/stats/",function(req,res,next){
+          
+          dbStats.find({},function(err,doc){
+          
+              res.send(doc);    
+              
+              
+          });
+          
+          
       });
-      //next();
-    });
-    // publish local stories
-
-    app.get("/api/stories", function(req, res) {
-      //console.log(req.url);
-      var param = {};
-      param.storyId = req.query.storyId;
-      param.showOnHomePage = req.query.showOnHomePage;
-
-      var message = "";
-
-      if (param.storyId == undefined) message = "invalid story Id";
-      if (param.showOnHomePage == undefined) param.showOnHomePage = 0;
-
-      if (message != "") {
-        // console.log(req.query);
-        res.send(req.query);
-        res.end();
-        return;
-      }
-      data.getStories(
-        param,
-
-        function(results) {
+      app.get("/api/users",function(req,res,next){
+      
+      dal.getUsers(function(err,results){
+          res.setHeader('Content-Type', 'application/json');
           res.send(results);
           res.end();
-        },
-        function(err) {
-          console.log(err);
-          res.render(err);
-        }
-      );
-    });
-
-    app.get("/api/getStory/", function(req, res) {
-      //console.log(req.url);
-      var param = {};
-      param.storyId = req.query.storyId;
-
-      var message = "";
-
-      if (param.storyId == undefined) message = "invalid story Id";
-
-      if (message != "") {
-        // console.log(req.query);
-        res.send(req.query);
-        res.end();
-        return;
-      }
-      data.getStory(
-        param,
-
-        function(results) {
-          res.send(results);
-          res.end();
-        },
-        function(err) {
-          // console.log(err);
-          res.render(err);
-        }
-      );
-    });
-
-    // get story comments
-    app.get("/api/StoryComments/:storyId", function(req, res) {
-      var storyId = req.params.storyId;
-      console.log(storyId);
-      var option = {};
-      option.storyId = storyId;
-      data.getStoryComments(option, function(err, results) {
-        if (err == null) res.send(results);
-        else res.send(err);
-        // res.end();
+      
+      
+      });  
+         
+          
       });
-    });
-    // end story comments
-    // save comments
-
-    app.get("/api/SaveComments/:storyId/:parent/:comments", function(
-      req,
-      res,
-      next
-    ) {
-      if (req.user == undefined || req.user == null) {
-        res.send({
-          error: "invalid user details",
-          errorMessage: "Please login to upload comments"
-        });
-        return;
-      }
-
-      var storyId = req.params.storyId;
-
-      var commentData = {};
-      commentData.storyId = storyId;
-      commentData.parent = req.params.parent;
-      commentData.username = req.user.username;
-      commentData.comments = req.params.comments;
-      commentData.created = new Date();
-
-      data.saveStoryComments(commentData, function(err, results) {
-        if (err == null) {
-          res.send(results);
-        } else {
-          console.log(err);
-          return next(err);
-        }
+      
+      // get weather
+      app.get("/api/kathuaWeather",function(req,res){
+          res.send({html:'weather'});
+       });
+       // error handling..
+       app.get("/errorpage/",function(req,res,next){
+          
+          //res.send({html:'done'});
+          
+          return next (new Error('error..'));
+          //return next();
+          
+          
+       },function(req,res,next){
+           
+           return next();
+           
+       },function(req,res){
+           
+           res.send('done');
+           
+       });
+       
+       app.post("/contactus",function(req,res){
+           
+          console.log('email %s',JSON.stringify(req.body));
+         //  dal.saveMessage(req.body,function(err,res){
+             //  param=res;
+               emailHelper.sendEmail(
+          {
+              from: req.body.email,
+              to: 'navs@hotmail.co.uk',
+              subject: 'mykathua.com->message from ' + req.body.name,
+              html: req.body.message
       });
-    });
+               
+               
+         //  },function (err) {
+               
+             //  console.log(err);
+             //  param.message=err.message;
+          // });
+      
+           res.send({code:200,message:'message sent successfully'});
+           
+       });
+  
+      // test
+       app.get("/test/executesql/",function(req,res){
+           
+          
+           dal.executeSql(function(err,msg){
+           if (err==null)
+           res.send(msg);
+           else
+           res.send(err);
+               
+           });
+          
+           
+           
+           
+           
+       });
+  
+}
 
-    app.get("/api/UpVoteStoryComment/:storyId/:username", function(
-      req,
-      res,
-      next
-    ) {
-      //console.log(req.url);
-      var storyId = req.params.storyId;
-
-      var commentData = {};
-      commentData.storyId = storyId;
-
-      commentData.username = req.params.username;
-
-      data.upVoteStoryComment(
-        commentData,
-        function(results) {
-          res.send(results);
-          //res.end();
-        },
-        function(err) {
-          console.log(err);
-          //res.send(err);
-          return next(err);
-        }
-      );
-    });
-
-    // get story comments
-    app.get("/api/getTweets/", function(req, res, next) {
-      try {
-        twt.getTweets(function(data) {
-          console.log(data);
-          res.send(data);
-        });
-      } catch (err) {
-        return next(err);
-      }
-    });
-
-    app.get("/callback", function(req, res) {
-      console.log("cb from twitter");
-      //res.send('call back from twitter');
-      //res.end();
-    });
-
-    app.get("/api/videos/:video?", function(req, res) {
-      //console.log(req.params.video);
-      videos.search({ searchStr: req.params.video || "kathua" }, function(
-        data
-      ) {
-        res.send(data);
-      });
-    });
-  };
 })(module.exports);
